dff35c5beef1f2f9987bc1e11cedfa140ff3b740
pritam guha <pritam@tenxerlabs.com>
2024-04-09 19:01:06 +0530

    added some array method

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
-function Square() {
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
   return (
-    <button className='bg-gray-300 border border-black h-20 w-20 m-1 leading-9 text-lg'>X</button>
-  )
+    <button onClick={onSquareClick} className='bg-gray-300 border border-black h-20 w-20 m-1 leading-9 text-lg'>{value}</button>
+  );
 }
 
 export default function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null))
+
+  function handleClick() {
+    const nextSquare = square.slice();
+    nextSquare[0] = "X";
+    setSquare(nextSquare)
+  }
+
   return (
     <>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className='flex'>
+        <Square value={square[0]} onSquareClick={handleClick} />
+        <Square value={square[1]} />
+        <Square value={square[2]} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className='flex'>
+        <Square value={square[3]} />
+        <Square value={square[4]} />
+        <Square value={square[5]} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className='flex'>
+        <Square value={square[6]} />
+        <Square value={square[7]} />
+        <Square value={square[8]} />
       </div>
     </>
   );
