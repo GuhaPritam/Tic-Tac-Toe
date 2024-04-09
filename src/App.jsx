24ebea8e9811fb1c1b4a8aa796990ba4378f2414
pritam guha <pritam@tenxerlabs.com>
2024-04-09 14:57:30 +0530

    created square function and use that in the board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-export default function App() {
+function Square() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <button className='bg-gray-300 border border-black h-20 w-20 m-1 leading-9 text-lg'>X</button>
   )
-} 
+}
+
+export default function Board() {
+  return (
+    <>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Board from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Board />
+  </React.StrictMode>
+);
