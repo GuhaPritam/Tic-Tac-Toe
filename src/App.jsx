4f1cf5163fd82e27a7f790ce428585a9fe8203f0
pritam guha <pritam@tenxerlabs.com>
2024-04-10 11:27:39 +0530

    remove hardcoded elements and pass through handleclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Square({ value, onSquareClick }) {
   return (
@@ -10,10 +10,10 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [square, setSquare] = useState(Array(9).fill(null))
 
-  function handleClick() {
+  function handleClick(i) {
     const nextSquare = [...square];
     console.log(nextSquare)
-    nextSquare[0] = "X";
+    nextSquare[i] = "X";
     setSquare(nextSquare)
   }
 
@@ -22,19 +22,19 @@ export default function Board() {
       <div>
         <p className='flex justify-center text-gray-200 font-bold text-[3rem] underline pb-6'>Tic Tac Toe</p>
         <div className='flex justify-center'>
-          <Square value={square[0]} onSquareClick={handleClick} />
-          <Square value={square[1]} />
-          <Square value={square[2]} />
+          <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={square[2]} onSquareClick={() => handleClick(2)} />
         </div>
         <div className='flex justify-center'>
-          <Square value={square[3]} />
-          <Square value={square[4]} />
-          <Square value={square[5]} />
+          <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={square[5]} onSquareClick={() => handleClick(5)} />
         </div>
         <div className='flex justify-center'>
-          <Square value={square[6]} />
-          <Square value={square[7]} />
-          <Square value={square[8]} />
+          <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={square[8]} onSquareClick={() => handleClick(8)} />
         </div>
       </div>
     </>
