d3cd864775f4948e57721a601e1045c95bd8c345
pritam guha <pritam@tenxerlabs.com>
2024-04-10 21:18:39 +0530

    added winner function

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,13 @@ export default function Board() {
   const [square, setSquare] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const winner = calculateWinner(square);
+  // const status;
+  // winner ? status = `Winner : ${winner}` : status = `New Player : ${xIsNext ? 'X' : 'O'}`;
+  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
   function handleClick(i) {
-    if (square[i]) {
+    if (square[i] || winner) {
       return;
     }
     const nextSquare = [...square];
@@ -24,7 +29,7 @@ export default function Board() {
   return (
     <>
       <div>
-        <p className='flex justify-center text-gray-200 font-bold text-[3rem] underline pb-6'>Tic Tac Toe</p>
+        <p className='flex justify-center text-gray-200 font-bold text-[3rem] underline pb-6'>{status}</p>
         <div className='flex justify-center'>
           <Square value={square[0]} onSquareClick={() => handleClick(0)} />
           <Square value={square[1]} onSquareClick={() => handleClick(1)} />
@@ -43,6 +48,26 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(square) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (square[a] && square[a] === square[b] && square[a] === square[c]) {
+      return square[a];
+    }
+  }
+  return null;
 }
 
 Square.propTypes = {
